d4b622a09f9f634ea97811ea2a867f13b5b12501
Greg Cheadle <gcheadle@gmail.com>
2013-11-12 10:41:59 -0500

    remove keys

--- amazon.js
+++ amazon.js
@@ -1,5 +1,5 @@
 module.exports = {
   provider: 'amazon',
-  keyId: 'AKIAJ5EV7WO5RAZMWLZQ',
-  key: 'U8GYSE7tnOi/mbT24YajSHSgo1kC/nn5FO/Id9nv'
+  keyId: '',
+  key: ''
 }
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,21 @@ angular.module('cloudstore')
       { name: 'Azure', id: 3 }
     ];
 
+    $scope.selectedProvider = {};
+    $scope.selectedContainer = {};
+
     $scope.getContainers = function() {
       $http.get('/containers', { params: { providerName: $scope.selectedProvider.name } }).success(function(data) {
         $scope.containers = data.containers;
+        $scope.onContainerSelected();
       });
+    };
+
+    $scope.onContainerSelected = function () {
+      $scope.uploader.formData = [{
+        providerName: $scope.selectedProvider.name,
+        containerName: $scope.selectedContainer.name
+      }];
     };
 
     // $scope.getFiles = function() {
@@ -26,50 +37,17 @@ angular.module('cloudstore')
     //   });
     // };
 
-    $scope.onContainerSelected = function () {
-      $scope.uploader.formData = [{
-        providerName: $scope.selectedProvider.name,
-        containerName: $scope.selectedContainer.name
-      }];
-    };
-
     var uploader = $scope.uploader = $fileUploader.create({
-      scope: $scope, // to automatically update the html. Default: $rootScope
+      scope: $scope,
       url: '/upload'
     });
 
-    // REGISTER HANDLERS
-
-    // uploader.bind('afteraddingfile', function (event, item) {
-    //   $log.log('After adding a file', item);
-    // });
-
-    // uploader.bind('afteraddingall', function (event, items) {
-    //   $log.log('After adding all files', items);
-    // });
-
-    // uploader.bind('changedqueue', function (event, items) {
-    //   $log.log('Changed queue', items);
-    // });
+    uploader.bind('afteraddingfile', function (event, item) {
+      $log.log('After adding a file', item);
+    });
 
     // uploader.bind('beforeupload', function (event, item) {
     //   $log.log('Before upload', item);
-    // });
-
-    // uploader.bind('progress', function (event, item, progress) {
-    //   $log.log('Progress: ' + progress, item);
-    // });
-
-    // uploader.bind('success', function (event, xhr, item) {
-    //   $log.log('Success: ' + xhr.response, item);
-    // });
-
-    // uploader.bind('complete', function (event, xhr, item) {
-    //   $log.log('Complete: ' + xhr.response, item);
-    // });
-
-    // uploader.bind('progressall', function (event, progress) {
-    //   $log.log('Total progress: ' + progress);
     // });
 
     // uploader.bind('completeall', function (event, items) {
